5f4b662d1f7481cf0380dd3bebd0f80784bb786e
Rodrigo <ingenieria.rlopez@gmail.com>
2022-11-29 22:46:32 -0300

    dummy commit

--- contracts/DuroCoin.sol
+++ contracts/DuroCoin.sol
@@ -4,6 +4,7 @@ pragma solidity >0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
 contract DuroCoin is ERC20 {
     constructor(uint256 initialSupply) ERC20("DuroCoin", "Duro"){
         _mint(msg.sender, initialSupply);
